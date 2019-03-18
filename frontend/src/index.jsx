3d3a4cbbe4b180f7db089e12044073a7f1ec64f9
Mikko Nikkanen <mikko.a.nikkanen@gmail.com>
2019-03-18 17:05:02 +0200

    Frontend data in list

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -9,42 +9,38 @@ const baseURL = process.env.ENDPOINT;
 
 /****** ADD YOUR CODE AFTER THIS LINE ******/
 
-const getGreetingFromBackend = async () => {
+const getSensorsFromBackend = async () => {
   try {
-    const url = `${baseURL}/api/greeting`
-    console.log("Getting greeting from "+url)
+    const url = `${baseURL}/api/sensors`
+    console.log("Getting sensor data from "+url)
     const response = await fetch(url);
     return response.json()
   } catch (error) {
     console.error(error);
   }
-  return { greeting :"Could not get greeting from backend"};
+  return {results: []};
 };
-
-
-const BackendGreeting = (props) => (
-  <div><p>Backend says: {props.greeting}</p></div>
-);
-
 
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      greeting: "",
+      sensors: [],
     };
   }
 
   async componentWillMount() {
-    const response = await getGreetingFromBackend();
-    this.setState({greeting: response.greeting});
+    const response = await getSensorsFromBackend();
+    this.setState({sensors: response.results});
   }
 
   render() {
 
     return (
-      <BackendGreeting greeting={this.state.greeting} />
+      <ul>
+        {this.state.sensors.map(sensor => <li key={sensor.id}>{sensor.timestamp}</li>)}
+      </ul>
     );
   }
 }
